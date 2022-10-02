514a2ad9ee23075648d51d1ae6eb2d667eab3c73
tamago572 <110117095+tamago572@users.noreply.github.com>
2022-10-02 21:28:23 +0900

    Initial Commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by dev on 2022/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by dev on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
