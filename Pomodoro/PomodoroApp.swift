f91666d83bc85f1872127bbb40981f90dcd917ae
tamago572 <110117095+tamago572@users.noreply.github.com>
2022-10-04 18:39:43 +0900

    初回起動時に実行するコードを削除

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -2,8 +2,6 @@ import SwiftUI
 
 @main
 struct PomodoroApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate  // この行を追加する
-    
     
     var body: some Scene {
         WindowGroup {
@@ -13,18 +11,4 @@ struct PomodoroApp: App {
     }
 }
 
-// 追加
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        
-       
-        // 起動時に1回だけやる処理をここに記述する
-        
-        
-        
-        return true
-    }
-}
 
